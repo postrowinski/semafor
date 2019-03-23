b35d5c9343514be85f4f80c1a7504416058a3408
Przemek <p.ostrowinski@gmail.com>
2019-03-23 11:24:59 +0100

    semafor z podstawowa logika

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
+Vue.component('v-select', VueSelect.VueSelect);
+
 const app = new Vue({
     el: '#app',
     data: () => ({
-        test: 'test'
-    })
+        signals: signals.map(signal => signal.id),
+        selected: null,
+        activeSignal: null,
+    }),
+    methods: {
+        onSelectChange: function() {
+            if (this.selected) {
+                this.activeSignal = signals.filter(signal => signal.id === this.selected)[0];
+            } else {
+                this.activeSignal = null;
+            }
+            console.log(this.activeSignal);
+        }
+    }
 });
